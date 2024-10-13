7fccb8745cbc17a5779c6cb6bd37d60581e526b7
Flavio Ramos <flavio.ramos@gmail.com>
2024-10-13 12:51:49 -0300

    fix pós feedback do professor

--- b3-scrapper.py
+++ b3-scrapper.py
@@ -5,15 +5,16 @@ import os
 import boto3
 import zipfile
 import pandas as pd
+import base64
+import csv
+import io
 from tqdm import tqdm
 
 parquets_dir = ""
-output_zip_file = ""
 output_parquet_file = ""
-date_string = ""
 access_key_id = ""
 secret_access_key = ""
-
+csv_data = ""
 
 def check_csv_file():
     global access_key_id
@@ -44,63 +45,60 @@ def check_csv_file():
         access_key_id, secret_access_key = lines[1].strip().split(',')
 
 
-def download_b3_zip():
-    global output_zip_file
-    global date_string
 
-    # get last work day
-    now = datetime.datetime.now()
-    day = now.date() - datetime.timedelta(days=1)
-    while day.weekday() >= 5:
-        day -= datetime.timedelta(days=1)
-    date_string = day.strftime('%Y-%m-%d')
-
+def download_csv():
+    global csv_data
+    
     downloads_dir = f'{os.getcwd()}/downloads'
-    output_zip_file = f'{downloads_dir}/{date_string}.zip'
-    url = f'https://arquivos.b3.com.br/rapinegocios/tickercsv/{date_string}'
+    output_csv_file = f'{downloads_dir}/bovespa.csv'
+    url = f'https://sistemaswebb3-listados.b3.com.br/indexProxy/indexCall/GetDownloadPortfolioDay/eyJpbmRleCI6IklCT1YiLCJsYW5ndWFnZSI6InB0LWJyIn0='
 
     # start download
-    response = requests.get(url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))
+    response = requests.get(url)
 
     # check for /downloads dir
     if not os.path.exists(downloads_dir):
         os.makedirs(downloads_dir)
     
+    base64_data = response.text
 
-    # write file
-    with open(output_zip_file, 'wb') as file, tqdm(
-        desc=f'Downloading {url}',
-        total=total_size,
-        unit='B',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        for data in response.iter_content(chunk_size=1024):
-            file.write(data)
-            bar.update(len(data))
+    csv_data = base64.b64decode(base64_data).decode('iso-8859-1')
+
+
+def process_csv():
+    global csv_data
+
+    lines = csv_data.strip().split('\r\n')
+
+    date = lines[0].split(' ')[-1]
+
+    lines[1] = f'Data;{lines[1]}'
+
+    lines = lines[1:-2]
+
+    for i in range(1, len(lines)):
+        lines[i] = f'{date};{lines[i]}'
+
+    csv_data = '\n'.join(lines)
+
 
 def generate_parquet():
+    global csv_data
     global output_parquet_file
     
-    parquets_dir = f'{os.getcwd()}/raw'
-    output_parquet_file = f'{parquets_dir}/{date_string}.parquet'
+    parquets_dir = f'{os.getcwd()}/parquet'
+    output_parquet_file = f'{parquets_dir}/bovespa.parquet'
 
     # check for /parquets dir
     if not os.path.exists(parquets_dir):
             os.makedirs(parquets_dir)
 
     print(f'Reading .csv ...')
-    # extract csv
-    with zipfile.ZipFile(output_zip_file, 'r') as zip_ref:
-        csv_filename = zip_ref.namelist()[0]
-        with zip_ref.open(csv_filename) as csv_file:
-            # read into pandas
-            df = pd.read_csv(csv_file, sep=';')
-            print(df)
+    df = pd.read_csv(io.StringIO(csv_data), sep=';')
+    print(df)
 
     # save parquet
-    print(f'Generating {date_string}.parquet ...')
+    print(f'Generating bovespa.parquet ...')
     df.to_parquet(output_parquet_file, index=False)
 
 def upload_to_s3():
@@ -108,8 +106,8 @@ def upload_to_s3():
     global secret_access_key
     global output_parquet_file
 
-    bucket = "fiap-2024-flavio-mlet"
-    prefix = "parquets"
+    bucket = "fiap-flavio-mlet-modulo-2"
+    prefix = "raw"
 
     session = boto3.Session(
         aws_access_key_id=access_key_id,
@@ -117,8 +115,8 @@ def upload_to_s3():
     )
     s3_client = session.client('s3')
 
-    print(f'Uploading {date_string}.parquet to s3://{bucket}/{prefix}/ ...')
-    s3_client.upload_file(output_parquet_file, bucket, f'{prefix}/{date_string}.parquet')
+    print(f'Uploading bovespa.parquet to s3://{bucket}/{prefix}/ ...')
+    s3_client.upload_file(output_parquet_file, bucket, f'{prefix}/bovespa.parquet')
     print(f'Upload complete, looking into bucket to confirm upload ...')
 
     paginator = s3_client.get_paginator('list_objects_v2')
@@ -126,7 +124,7 @@ def upload_to_s3():
     found = False
     for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
         for obj in page.get('Contents', []):
-            if obj['Key'] == f'{prefix}/{date_string}.parquet':
+            if obj['Key'] == f'{prefix}/bovespa.parquet':
                 found = True
 
     if found:
@@ -137,6 +135,7 @@ def upload_to_s3():
 
 
 check_csv_file()
-download_b3_zip()
+download_csv()
+process_csv()
 generate_parquet()
 upload_to_s3()
